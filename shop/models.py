393c0088dc3de0716556165a43b88e44dbab3fe6
hadiagha07 <hadigholamii077@gmail.com>
2025-01-17 11:19:09 +0330

    add CategoryMenu

--- shop/models.py
+++ shop/models.py
@@ -3,6 +3,40 @@ from django.utils.text import slugify
 from django.utils import timezone
 
 
+class CategoryMenu(models.Model):
+    name = models.CharField(max_length=100, verbose_name='اسم')
+
+    class Meta:
+        verbose_name = 'منوی هدر'
+        verbose_name_plural = 'منوی هدر'
+
+    def __str__(self):
+        return self.name
+
+class ItemCategoryMenu(models.Model):
+    name = models.CharField(max_length=100, verbose_name='اسم')
+
+
+
+class SubCategory(models.Model):
+    category = models.ForeignKey(ItemCategoryMenu, related_name='subcategories', on_delete=models.CASCADE, verbose_name='موضوع')
+    name = models.CharField(max_length=100)
+    url = models.URLField(verbose_name='لینک')
+
+    class Meta:
+        verbose_name = 'موضوع های منو'
+        verbose_name_plural = 'موضوع های منو'
+
+    def __str__(self):
+        return self.name
+
+class Color(models.Model):
+    name = models.CharField(max_length=50)
+    hex_code = models.CharField(max_length=7)
+
+    def __str__(self):
+        return self.name
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True, verbose_name="اسم دسته‌بندی")
@@ -29,6 +63,10 @@ class Product(models.Model):
     name = models.CharField(max_length=400, verbose_name='اسم محصول')
     nickname = models.CharField(max_length=400, verbose_name='اسم محصول به انگلیسی')
     product_code = models.CharField(max_length=6, unique=True, blank=True, verbose_name="کد محصول")
+    colors = models.ManyToManyField(Color)
+    price_before_dis = models.PositiveBigIntegerField(default=0, blank=True, null=True, verbose_name='قیمت')
+    price = models.PositiveBigIntegerField(default=0, blank=True, null=True, verbose_name='قیمت')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="تاریخ ایجاد")
 
     class Meta:
         verbose_name = "محصول"
@@ -38,5 +76,21 @@ class Product(models.Model):
         return self.name
 
 
+class Feature(models.Model):
+    product = models.ForeignKey(Product, verbose_name='محصول', related_name='features', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, verbose_name='اسم ویزگی')
+    value = models.CharField(max_length=100, verbose_name='ولیو ویزگی')
 
+    def __str__(self):
+        return f"{self.name}: {self.value}"
+
+
+class Image(models.Model):
+    post = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images', verbose_name='محصول')
+    image_file = models.ImageField(upload_to="uploads/%Y/%m/", blank=True, null=True, verbose_name='فایل تصویر')
+    created = models.DateTimeField(auto_now=True, verbose_name='تاریخ ایجاد')
+
+
+class Size(models.Model):
+    size = models.IntegerField(verbose_name='سایز کفش')
 
